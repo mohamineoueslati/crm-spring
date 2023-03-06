82179865ba9e372be8926d6c1194dbab2dd35b1e
Mohamed Amine Oueslati <mohamad.amine.oueslati@gmail.com>
2023-03-06 09:13:04 +0100

    adding controller advice

---
+++ src/main/java/com/moh/crmspring/handler/ControllerExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.moh.crmspring.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+}
